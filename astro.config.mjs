2f2abfa1293de1d782e83cbfb04ed3931b40bdd4
paopi <paopi@users.noreply.github.com>
2026-07-05 18:26:39 +0900

    Fix search indexing configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ichiryuteihen-blog.pages.dev',
+  site: 'https://ichiryuteihen.com',
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
